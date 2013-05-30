e39b4c44ddd057e44f712eb0cb967b40fb24cce4
pMan <prasadcnair@gmail.com>
2013-05-30 22:27:37 +0530

    added listbox of all enable dics, variables renamed

--- js/inject.js
+++ js/inject.js
@@ -1,55 +1,91 @@
-
 // setup iframe to show the popup
-var iframe = document.createElement("iframe");
-iframe.setAttribute("class", "iframe_window");
+var __liframe = document.createElement("iframe");
+__liframe.setAttribute("id", "__iframe_window");
 
 // setup iframe container. If undefined, create one. Empty existing one, otherwise
 if( iframe_container == undefined) {
 	var iframe_container = document.createElement("div");
 	iframe_container.setAttribute("name", "lookup_definitions_online_container");
-	iframe_container.setAttribute("class", "lookup_popup");
+	iframe_container.setAttribute("id", "__lookup_popup");
 }else {
 	iframe_container.innerHTML = "";
 }
 
-// setup styles for popup container. Popup height is 60% of client area height and
-// left is 10% of client area width
-var height = window.innerHeight * 0.6;
-var left = window.innerWidth * 0.1;
-iframe_container.setAttribute("style", "height:"+height+"px;left:"+left+"px;z-index:16777270;");
+// setup styles for popup container.Parameters are set based on the
+// dimensions of client area.
+var __lh = "80%"; // height
+var __lw = "80%"; // width
+var __lt = "10%"; // top
+var __ll = "10%"; // left
+
+iframe_container.setAttribute("style", "height:"+__lh+";left:"+__ll+";width:"
+		+__lw+";top:"+__lt+";z-index:16777270;");
 
 // creating the close button
-var close = document.createElement("div");
-close.innerHTML = "Close";
-close.setAttribute("class", "close_lookup");
+var __lclose = document.createElement("div");
+__lclose.innerHTML = "X";
+__lclose.setAttribute("id", "__close_lookup");
+
+// seggregate the date sent from extension script
+var __ldata = $.parseJSON(a);
+var __lurl = __ldata[0];
+var __lkwd = __ldata[1];
+var __ldics = __ldata[2];
+
+// create the select box to switch between dictionaries
+var __lsel = document.createElement("select");
+__lsel.setAttribute("id", "__lsel"); 
+
+// add all dictionaries which are enabled by user.
+for (var i=0; i < __ldics.length; i++) {
+	if ( __ldics[i].func == "UrbanDict" || __ldics[i].func == "Definition" || __ldics[i].func == "" ) {
+		continue;
+	}
+    var ov = document.createElement("option");
+    ov.value = __ldics[i].url + __lkwd;
+    ov.text = __ldics[i].title;
+    if (ov.value == __lurl) {
+    	ov.setAttribute("selected", "selected");
+    }
+    __lsel.appendChild(ov);
+}
 
 // add iframe and close button to the containder
-iframe_container.appendChild(iframe);
-iframe_container.appendChild(close);
+iframe_container.appendChild(__liframe);
+iframe_container.appendChild(__lclose);
+iframe_container.appendChild(__lsel);
 
 // add container to the document body
 document.body.appendChild(iframe_container);
 
 // set 'loading...' in the popup and then set url
-iframe.contentDocument.write("<html><body><div style='margin:40px auto; "+
-	"display:block; color: gray;'><i>loading...</i></div></body></html>");
-iframe.setAttribute("src", url);
+__liframe.contentDocument.write("<html><body><div style='width:100%; "+
+	"color: gray; text-align: center; line-height: 100%;'><i style='padding-top: 100px;"+
+	"display: block;'>loading...</i></div></body></html>");
+__liframe.setAttribute("src", __lurl);
 
 // finally show it!
-$(".lookup_popup").fadeIn("1600");
+$("#__lookup_popup").fadeIn("1600");
 
-// close button action
+// close popup by Esc press listener
 $(document).keyup(function(e) {
     if (e.keyCode == 27) { // Esc
-        $(".lookup_popup").fadeOut("600", function(){
-        	$(".lookup_popup").remove();
+        $("#__lookup_popup").fadeOut("600", function(){
+        	$("#__lookup_popup").remove();
         });
     }
 });
 
-$(".close_lookup").click(function() {
-	$(".lookup_popup").fadeOut("600", function(){
-		$(".lookup_popup").remove();
+// Close popup button click hadler
+$("#__close_lookup").click(function() {
+	$("#__lookup_popup").fadeOut("600", function(){
+		$("#__lookup_popup").remove();
 	});
 });
 
+// Other dictionaries change handler
+$("#__lsel").change(function() {
+	if ($(this).val() != "") { // select other dics
+		__liframe.setAttribute("src", $(this).val());
+	}
+});
