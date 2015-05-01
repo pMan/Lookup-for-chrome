423a8875043396967b971c27fbcab603ca23b6ec
pMan <pman@karmicbee.com>
2015-05-01 12:37:03 +0530

    Added macmillan american dict, replaced all site permission with just dict sites

--- js/helpers.js
+++ js/helpers.js
@@ -196,7 +196,7 @@ var msg = '<span></span>'; // if any, once 'Save' clicked
 			var dics = enabledDics.split(','); // private
 		} else {
 			// for the first time, after installation
-			var dics = [0,1,2,3,4,5,6,7,8,9,11,12,13,15,16,17,18,19,20,21,22,23,24];
+			var dics = [0,1,2,3,4,5,6,7,8,9,11,12,13,15,16,17,18,19,20,21,22,23,24,26];
 		}
 		if (flag == "menu") {
 			var retVal = [];
